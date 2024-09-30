1c0081debc9bd8e8146758c66080fce2a0d9ab1a
s4r44d43 <sararez@live.it>
2024-09-30 10:17:08 +0200

    start

---
+++ shoppiece-front-end/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import HelloShoppiece from './components/HelloShoppiece.vue'
+export default {
+  components: {
+    HelloShoppiece
+  }
+}
+</script>
+
+<template>
+  <HelloShoppiece/>
+</template>
+
+<style scoped>
+</style>
---
+++ shoppiece-front-end/src/components/HelloShoppiece.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+  name: 'HelloShoppiece'
+}
+</script>
+
+<template>
+  <h1>
+    HELLO SHOPPIECE
+  </h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
